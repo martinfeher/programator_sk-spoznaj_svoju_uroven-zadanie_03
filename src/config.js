708d82e07ab3688feb8ab1cfc8e3bc6897561b5e
LAPTOP-Q85D7HMG\Martin <mn.feher@gmail.com>
2021-10-15 08:41:29 +0200

    update data server order

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const config = 
     {
-        "APP_ENV": process.env.REACT_APP_ENV === "production" ?  "production" : "local",
-        "API_SERVER_URL": process.env.REACT_APP_ENV === "production" ?  "http://api.programator.sk" : "http://localhost:5000",
-        "IMAGE_URL_PATH": process.env.REACT_APP_ENV === "production" ?  "/images/300x300" : "",
+        "APP_ENV": process.env.REACT_APP_ENV === "local" ? "local" : "production",
+        "API_SERVER_URL": process.env.REACT_APP_ENV === "local" ? "http://localhost:5000" : "http://api.programator.sk",
+        "IMAGE_URL_PATH": process.env.REACT_APP_ENV === "local" ? "" : "/images/300x300",
         "API_LOCAL_SERVER_URL": "http://localhost:5000",
         "API_PRODUCTION_SERVER_URL": "http://api.programator.sk"
     }
